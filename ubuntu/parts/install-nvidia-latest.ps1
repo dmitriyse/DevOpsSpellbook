34b9846637d971c5a3f38a090dddeb30428efe15
dse <dse@sys-plex.com>
2017-11-04 00:29:08 +0400

    install-nvidia-latest added

---
+++ ubuntu/parts/install-nvidia-latest.ps1
@@ -0,0 +1,14 @@
+#!/bin/pwsh
+
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo apt update -y
+
+# This is undocumented dependency that is required to all nvidia dependent packages correctly installed
+sudo apt install nvidia-modprobe -y
+
+
+$available_driver_versions = apt-cache search nvidia- | where { $_ -match 'nvidia-\d+ .*'} | %{ [int]$_.Substring("nvidia-".Length,3)}
+$latest_driver_version = $available_driver_versions | sort | Select-Object -Last 1
+
+# Also this installation was performed with plugged nvidia GPU. Probably this is not reuired, experiments needed.
+sudo apt install nvidia-$latest_driver_version libcuda1-$latest_driver_version --no-install-recommends -y
